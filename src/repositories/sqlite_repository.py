5ef14632a58e7e26dc4fad6496a0e580e4487268
Stendal <ramon@bpconsultoria.info>
2026-01-20 17:09:57 -0300

    cleaning sqlite repository

--- src/repositories/sqlite_repository.py
+++ src/repositories/sqlite_repository.py
@@ -1,4 +1,4 @@
-from typing import List, Optional
+from typing import List, Optional, Dict, Any
 from sqlalchemy.orm import Session
 from models.models import Antecipacao, get_session
 
@@ -14,32 +14,13 @@ class SQLiteRepository:
         self.session.refresh(antecipacao)
         return antecipacao
     
-    def save_bulk(self, antecipacoes: List[Antecipacao]) -> List[Antecipacao]:
-        self.session.add_all(antecipacoes)
-        self.session.commit()
-        for antecipacao in antecipacoes:
-            self.session.refresh(antecipacao)
-        return antecipacoes
-    
-    def get_by_id(self, antecipacao_id: int) -> Optional[Antecipacao]:
-        return self.session.query(Antecipacao).filter(Antecipacao.id == antecipacao_id).first()
-    
-    def get_all(self) -> List[Antecipacao]:
-        return self.session.query(Antecipacao).all()
-    
-    def get_not_inserted(self) -> List[Antecipacao]:
-        return self.session.query(Antecipacao).filter(Antecipacao.is_inserted == False).all()
-    
-    def get_by_cedente(self, cedente: str) -> List[Antecipacao]:
-        return self.session.query(Antecipacao).filter(Antecipacao.cedente == cedente).all()
-    
-    def get_by_sacado(self, sacado: str) -> List[Antecipacao]:
-        return self.session.query(Antecipacao).filter(Antecipacao.sacado == sacado).all()
-    
     def update(self, antecipacao: Antecipacao) -> Antecipacao:
         self.session.commit()
         self.session.refresh(antecipacao)
         return antecipacao
+    
+    def get_by_id(self, antecipacao_id: int) -> Optional[Antecipacao]:
+        return self.session.query(Antecipacao).filter(Antecipacao.id == antecipacao_id).first()
     
     def mark_as_inserted(self, antecipacao_id: int) -> bool:
         antecipacao = self.get_by_id(antecipacao_id)
@@ -48,3 +29,33 @@ class SQLiteRepository:
             self.session.commit()
             return True
         return False
+    
+    def insert_antecipacao(
+        self,
+        cedente: str,
+        sacado: str,
+        bordero: int,
+        cnpj_sacado: Optional[str] = None,
+        titulo: Optional[int] = None,
+        valor: Optional[float] = None,
+        vencimento: Optional[str] = None,
+        portal_email: Optional[str] = None,
+        login: Optional[str] = None,
+        senha: Optional[str] = None,
+        is_inserted: bool = False
+    ) -> Antecipacao:
+        
+        antecipacao = Antecipacao(
+            bordero=bordero,
+            cedente=cedente,
+            sacado=sacado,
+            cnpj_sacado=cnpj_sacado,
+            titulo=titulo,
+            valor=valor,
+            vencimento=vencimento,
+            portal_email=portal_email,
+            login=login,
+            senha=senha,
+            is_inserted=is_inserted
+        )
+        return self.save(antecipacao)
